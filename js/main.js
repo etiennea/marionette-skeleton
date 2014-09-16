40c6436ebf608399f6b52f9d41d63f0c121c13f3
Etienne Adriaenssen <etiennea@gmail.com>
2014-09-16 18:08:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log('cool');
-
 requirejs.config({
 	paths : {
 		backbone: '../bower_components/backbone/backbone',
@@ -17,8 +15,12 @@ requirejs.config({
 requirejs(['app',
 	'marionette',
 	'router',
-	'controller'
-	], function(app, Marionette, Router, Controller){
+	'controller',
+	'models',
+	'collections',
+	'templates',
+	'views'
+	], function(app, Marionette, Router, Controller, Models, Collections, Templates, Views){
 
 		"use strict";
 
@@ -54,7 +56,7 @@ requirejs(['app',
     			var href = cur.attr('href');
     			if(href && href == "#")
     				return e.preventDefault();
-    			if(href && !cur.hasClass('external') && !cur.hasClass('dropdown-toggle')){
+    			if(href && !cur.hasClass('external')){
     				e.preventDefault();
     				app.router.navigate(cur.attr('href'), {trigger: true});
     				$("main").delay(200).animate({ scrollTop: 0 }, 300);
@@ -63,18 +65,36 @@ requirejs(['app',
     	});
 
 		app.addInitializer(function(options){
+			app.baseUrl = api + '/1';
+			app.token = token;
+
+			app.addRegions({
+				header      : 'header',
+				main        : 'main',
+				footer      : 'footer'
+			});
+
+			app.models = Models;
+			app.collections = Collections;
+			app.views = Views;
+			app.events = {};
+			_.extend(app.events, Backbone.Events);
+
 			app.router = new Router({
-				controller : Controller
+				controller : Controller,
 			});
 		})
 
+		app.on("start", function(options){
+			if (Backbone.history){  Backbone.history.start({pushState: true}); }
+			app.header.show(new app.views.header());
+			app.footer.show(new app.views.footer());
+			app.account = new app.models.account();
+			app.account.fetch();
+		});
+
 		app.start();
 
-		app.account = new app.models.account();
-		app.account.fetch();
-		app.main.show(new app.views.main());
-		app.header.show(new app.views.header());
-		app.footer.show(new app.views.footer());
 
 
 	});
